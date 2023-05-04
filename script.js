f5969abac58b6d7a06d11f5d3f57f69e866fb0f1
Swadhin Ghosh <43653792+Swadhincse@users.noreply.github.com>
2023-05-05 01:21:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+window.addEventListener('resize', () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const sizeInfo = document.getElementById('sizeInfo');
+  const h1Tag = sizeInfo.querySelector('h1');
+  h1Tag.textContent = `Width: ${width} Height: ${height}`;
+});
